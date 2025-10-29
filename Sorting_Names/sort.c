e29bac238c2569017fee037516a5f737bf19cf51
Phinatech <c.ugbo@alustudent.com>
2025-10-29 21:25:40 +0200

    making some changes in sorting names

--- Sorting_Names/sort.c
+++ Sorting_Names/sort.c
@@ -5,94 +5,141 @@
 #define MAX_NAMES 20
 #define MAX_LENGTH 50
 
-// Function pointer type for comparison functions
+// Define function pointer type for comparison functions
 typedef int (*CompareFunc)(const char*, const char*);
 
-// Comparison function for ascending order
-int compareAscending(const char *name1, const char *name2) {
-    return strcmp(name1, name2);
+// ---------------------------
+// Case-insensitive comparison
+// ---------------------------
+int ciCmp(const char *a, const char *b)
+{
+    unsigned char ca, cb;
+    while (*a && *b)
+    {
+        ca = (unsigned char)tolower((unsigned char)*a);
+        cb = (unsigned char)tolower((unsigned char)*b);
+        if (ca != cb)
+            return (ca < cb) ? -1 : 1;
+        a++;
+        b++;
+    }
+    // Return difference if one string ends first
+    return (*a == *b) ? 0 : (*a ? 1 : -1);
 }
 
-// Comparison function for descending order
-int compareDescending(const char *name1, const char *name2) {
-    return strcmp(name2, name1);
+// Ascending and descending comparison functions
+int compareAscending(const char *name1, const char *name2)
+{
+    return ciCmp(name1, name2);
 }
 
-// Bubble sort function using function pointer
-void bubbleSort(char names[][MAX_LENGTH], int count, CompareFunc compare) {
-    int i, j;
+int compareDescending(const char *name1, const char *name2)
+{
+    return ciCmp(name2, name1);
+}
+
+// ---------------------------
+// Bubble sort implementation
+// ---------------------------
+void bubbleSort(char names[][MAX_LENGTH], int count, CompareFunc compare)
+{
     char temp[MAX_LENGTH];
-    
-    for (i = 0; i < count - 1; i++) {
-        for (j = 0; j < count - i - 1; j++) {
-            if (compare(names[j], names[j + 1]) > 0) {
+    int swapped;
+
+    for (int i = 0; i < count - 1; i++)
+    {
+        swapped = 0;
+        for (int j = 0; j < count - i - 1; j++)
+        {
+            if (compare(names[j], names[j + 1]) > 0)
+            {
                 // Swap names[j] and names[j + 1]
                 strcpy(temp, names[j]);
                 strcpy(names[j], names[j + 1]);
                 strcpy(names[j + 1], temp);
+                swapped = 1;
             }
         }
+        if (!swapped)
+            break; // Stop early if no swaps made
     }
 }
 
-// Function to convert string to lowercase for comparison
-void toLowerCase(char *str) {
-    for (int i = 0; str[i]; i++) {
-        str[i] = tolower(str[i]);
-    }
+// ---------------------------
+// Helper function to lowercase
+// ---------------------------
+void toLowerCase(char *str)
+{
+    for (int i = 0; str[i]; i++)
+        str[i] = (char)tolower((unsigned char)str[i]);
 }
 
-int main() {
+// ---------------------------
+// Main Program
+// ---------------------------
+int main(void)
+{
     char names[MAX_NAMES][MAX_LENGTH];
     char choice[10];
     CompareFunc compareFunction;
-    
+
     printf("=== NAME SORTING PROGRAM ===\n");
     printf("Please enter 20 full names:\n");
     printf("----------------------------\n");
-    
-    // Clear input buffer
-    getchar();
-    
-    // Input 20 names from user
-    for (int i = 0; i < MAX_NAMES; i++) {
+
+    // Input 20 names
+    for (int i = 0; i < MAX_NAMES; i++)
+    {
         printf("Enter name %d: ", i + 1);
-        fgets(names[i], MAX_LENGTH, stdin);
-        
+        if (!fgets(names[i], MAX_LENGTH, stdin))
+        {
+            fprintf(stderr, "Input error.\n");
+            return 1;
+        }
+
         // Remove newline character if present
         size_t len = strlen(names[i]);
-        if (len > 0 && names[i][len - 1] == '\n') {
+        if (len > 0 && names[i][len - 1] == '\n')
             names[i][len - 1] = '\0';
-        }
     }
-    
+
     printf("\n----------------------------\n");
     printf("Enter 'asc' for ascending order or 'desc' for descending order: ");
-    scanf("%s", choice);
-    
-    // Convert choice to lowercase for case-insensitive comparison
+    if (scanf("%9s", choice) != 1)
+    {
+        fprintf(stderr, "Invalid input.\n");
+        return 1;
+    }
+
     toLowerCase(choice);
-    
-    // Select comparison function based on user input
-    if (strcmp(choice, "asc") == 0) {
+
+    // Select function based on user choice
+    if (strcmp(choice, "asc") == 0)
+    {
         compareFunction = compareAscending;
         printf("\nSorting in ASCENDING order...\n");
-    } else if (strcmp(choice, "desc") == 0) {
+    }
+    else if (strcmp(choice, "desc") == 0)
+    {
         compareFunction = compareDescending;
         printf("\nSorting in DESCENDING order...\n");
-    } else {
-        printf("\nInvalid input! Defaulting to ASCENDING order...\n");
+    }
+    else
+    {
+        printf("\nInvalid choice! Defaulting to ASCENDING order...\n");
         compareFunction = compareAscending;
     }
-    
-    // Perform bubble sort using function pointer
+
+    // Sort names using bubble sort and function pointer
     bubbleSort(names, MAX_NAMES, compareFunction);
-    
+
+    // Print results
     printf("\nSorted names:\n");
     printf("----------------------------\n");
-    for (int i = 0; i < MAX_NAMES; i++) {
+    for (int i = 0; i < MAX_NAMES; i++)
+    {
         printf("%2d. %s\n", i + 1, names[i]);
     }
-    
+
     return 0;
 }
